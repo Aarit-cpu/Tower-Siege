d3d0bb0ea2a206c6471857e7f53d6df64448dac2
Aarit-cpu <76479740+Aarit-cpu@users.noreply.github.com>
2021-04-07 21:11:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,87 +1,146 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var holder,ball,ground;
-var stand1,stand2;
+var block1, block2, block3, block4, block5, block6, block7, block8, block9;
+var block10,block11,block12, block13, block14;
+var player;
+var ground1, ground2, ground3;
+
+var block15, block16, block17, block18, block19, block20, block21;
+var block22;
+var sling;
+var player;
+
 var ball;
-var slingShot;
-var polygon_img;
+var img;
+
 function preload(){
-  polygon_img=loadImage("polygon.png");
+  img = loadImage("hexagon (1).png");
+
 }
+
 function setup() {
-  createCanvas(900,400);
+  createCanvas(1500,600);
   engine = Engine.create();
-  world = engine.world;
-  Engine.run(engine);
-  ground = new Ground();
-  stand1 = new Stand(390,300,250,10);
-  stand2 = new Stand(700,200,200,10);
- 
-  //level one
-  block1 = new Block(300,275,30,40);
-  console.log(block1);
-  block2 = new Block(330,275,30,40);
-  block3 = new Block(360,275,30,40);
-  block4 = new Block(390,275,30,40);
-  block5 = new Block(420,275,30,40);
-  block6 = new Block(450,275,30,40);
-  block7 = new Block(480,275,30,40);
-  //level two
-  block8 = new Block(330,235,30,40);
-  block9 = new Block(360,235,30,40);
-  block10 = new Block(390,235,30,40);
-  block11 = new Block(420,235,30,40);
-  block12 = new Block(450,235,30,40);
-  //level three
-  block13 = new Block(360,195,30,40);
-  block14 = new Block(390,195,30,40);
-  block15 = new Block(420,195,30,40);
-  //top
-  block16 = new Block(390,155,30,40);
+    world = engine.world;
 
-  //stone
-  polygon1 = new Object(56,297,30,40)
 
-  
+  block1 = new Block(600,260,30,40);
+  block2 = new Block(570,260,30,40);
+  block3 = new Block(540,260,30,40);
+  block4 = new Block(630,260,30,40);
+  block5 = new Block(660,260,30,40);
+
+
+
+  block6 = new Block(585,220,30,40);
+  block7 = new Block(555,220,30,40);
+  block8 = new Block(615,220,30,40);
+  block9 = new Block(645,220,30,40);
+
+
+
+  bolck10 = new Block(600,170,30,40);
+  block11 = new Block(570,180,30,40);
+  block12 = new Block(630,180,30,40);
+
+
+  block13 = new Block(600,140,30,40);
+
+  ground1 = new Ground(600,285,200,10);
+  ground2 = new Ground(900,195,200,10);
+  ground3 = new Ground(750, 600, 1500, 10)
+
+
+  block14 = new Block(900,170,30,40);
+  block15 = new Block(930,170,30,40);
+  block16 = new Block(870,170,30,40);
+  block17 = new Block(840,170,30,40);
+  block18 = new Block(960,170,30,40);
+
+  block19 = new Block(900,140,30,40);
+  block20 = new Block(930,140,30,40);
+  block21 = new Block(870,140,30,40);
+
+  block22 = new Block(900,110,30,40);
+
+  player = new Player(50,200,30,30);
+
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  sling = new Chain(this.ball,{x:150, y:160});
 
 }
-function draw() {
-  background(56,44,44); 
- 
-  textSize(20);
-  fill("lightyellow");
-  
 
-  ground.display();
-  stand1.display();
-  
-  strokeWeight(2);
-  stroke(15);
-  fill("skyblue");
+function draw() {
+  background("grey");
+  Engine.update(engine);
+
+  fill(rgb(135, 205, 236));
+
   block1.display();
   block2.display();
   block3.display();
   block4.display();
   block5.display();
+
+  fill("lightBlue");
   block6.display();
   block7.display();
-  fill("pink");
   block8.display();
   block9.display();
-  block10.display();
+
+  fill("lightPink");
+  bolck10.display();
   block11.display();
   block12.display();
-  fill("turquoise");
+  fill(rgb(47, 48, 48));
+
   block13.display();
+
+  fill(rgb(135, 205, 236));
   block14.display();
   block15.display();
-  fill("grey");
   block16.display();
-  polygon1.display();
- 
+  block17.display();
+  block18.display();
+  fill("lightGreen");
 
+  block19.display();
+  block20.display();
+  block21.display();
+  fill("lime");
+
+  block22.display();
+
+  ground1.display();
+  ground2.display();
+  ground3.display();
+
+  imageMode(CENTER);
+  image(img,ball.position.x,ball.position.y,40,40);
+
+  sling.display();
+}
+
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  sling.fly();
+}
+
+function keyPressed(){
+	if(keyCode === 32)
+	{
+		Matter.Body.setPosition(player.body,{x:235, y:420})
+		chain.attach(player.body);
+	}
 }
